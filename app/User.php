e236212bcf261e0df60500ac75396f17aa8cfa8c
Marvin Bentinganan <marvbentinganan@gmail.com>
2018-07-24 16:27:40 +0800

    Updated User Model and RegisterController

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'username', 'email', 'password',
+        'firstname', 'lastname', 'username', 'email', 'password',
     ];
 
     /**
