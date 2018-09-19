f173bcf1794ad022051e3f3a015a96757b45a735
Marvin Bentinganan <marvbentinganan@gmail.com>
2018-09-19 10:35:02 +0800

    Updated Digihub Guidelines

--- resources/views/digihub/guidelines.blade.php
+++ resources/views/digihub/guidelines.blade.php
@@ -12,96 +12,62 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
+    <link href="{{ asset('plugins/ionicons/css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/semantic-ui/semantic.min.css') }}" rel="stylesheet">
     <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
+        body{
+            background: rgb(2,25,94); background: linear-gradient(120deg, rgba(2,25,94,1) 35%, rgba(9,29,121,0.8029411593739058) 57%,
+            rgba(5,71,148,1) 81%);
         }
     </style>
-    <link href="{{ asset('css/semantic-ui/semantic.min.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="title m-b-md">
-                <img src="{{ asset('images/digihub-logo.jpg') }}" alt="">
-            </div>
-            {{-- <div class="links">
-                <a href="#">Faculty Evaluation</a>
-                <a href="#">RCI WiFi</a>
-                <a href="#">DigiHub</a>
-                <a href="#">Events Attendance</a>
-            </div> --}}
-        </div>
-    </div>
-    <div class="ui sixteen wide coumn">
-        <div class="ui segment">
-            <h1 class="ui huge centered header">Usage Guidelines</h1>
-            <div class="ui relaxed divided list">
-                <div class="item">
-                    The use of this machine is limited to forty-five (45) minutes only per user. After the alloted time, the computer will automatically
-                    restart so please save your work.
+    <div class="ui hidden divider"></div>
+    <div class="ui grid container">   
+        <div class="centered eight wide middle aligned column">
+            <div class="ui raised fluid card">
+                <div class="image">
+                    <img src="{{ asset('images/digihub-logo.jpg') }}" alt="">
                 </div>
-                <div class="item">
-                    In order to protect our computer units from viruses and malwares, please avoid browsing unsecured sites.
-                </div>
-                <div class="item">
-                    Downloading of movies, music, and other pirated/unlicensed materials is discouraged.
-                </div>
-                <div class="item">
-                    A USB port is provided to save your files, please handle with care.
+                <div class="content">
+                    <div class="header">Guidelines</div>
+                    <div class="description">
+                        <div class="ui relaxed divided list">
+                            <div class="item">
+                                <i class="large ion-information-circled icon"></i>
+                                <div class="content">
+                                    <div class="header">Time Limit</div>
+                                    <div class="description">The use of this machine is limited to forty-five (45) minutes only per user. After the alloted time, the computer
+                                        will automatically restart so please save your work.</div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <i class="large ion-information-circled icon"></i>
+                                <div class="content">
+                                    <div class="header">Security</div>
+                                    <div class="description">In order to protect our computer units from viruses and malwares, please avoid browsing unsecured sites.</div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <i class="large ion-information-circled icon"></i>
+                                <div class="content">
+                                    <div class="header">Downloads</div>
+                                    <div class="description">Downloading of movies, music, and other pirated/unlicensed materials is discouraged.</div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <i class="large ion-information-circled icon"></i>
+                                <div class="content">
+                                    <div class="header">File Transfer</div>
+                                    <div class="description">A USB port is provided to save your files, please handle with care.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
-
 </html>
