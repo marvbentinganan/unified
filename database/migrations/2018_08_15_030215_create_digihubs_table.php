8ba04bc7b5124a41f57494a9a09feddefe806798
Marvin Bentinganan <marvbentinganan@gmail.com>
2018-11-17 14:42:10 +0800

    Removed Digihub Migration

--- database/migrations/2018_08_15_030215_create_digihubs_table.php
+++ database/migrations/2018_08_15_030215_create_digihubs_table.php
@@ -11,22 +11,22 @@ class CreateDigihubsTable extends Migration
      */
     public function up()
     {
-        Schema::create('digihubs', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('ip')->nullable();
-            $table->string('location')->nullable();
-            $table->string('image')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        // Schema::create('digihubs', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->string('name');
+        //     $table->string('ip')->nullable();
+        //     $table->string('location')->nullable();
+        //     $table->string('image')->nullable();
+        //     $table->timestamps();
+        //     $table->softDeletes();
+        // });
 
-        Schema::create('usages', function (Blueprint $table) {
-            $table->increments('id');
-            $table->unsignedInteger('digihub_id');
-            $table->foreign('digihub_id')->references('id')->on('digihubs')->onUpdate('cascade')->onDelete('cascade');
-            $table->timestamps();
-        });
+        // Schema::create('usages', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->unsignedInteger('digihub_id');
+        //     $table->foreign('digihub_id')->references('id')->on('digihubs')->onUpdate('cascade')->onDelete('cascade');
+        //     $table->timestamps();
+        // });
     }
 
     /**
@@ -34,7 +34,7 @@ class CreateDigihubsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usages');
-        Schema::dropIfExists('digihubs');
+        // Schema::dropIfExists('usages');
+        // Schema::dropIfExists('digihubs');
     }
 }
